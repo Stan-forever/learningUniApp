a984250604d911615c6fd40e1f16aa3d2aef1027
xujunsheng <1725116377@qq.com>
2024-02-29 17:39:45 +0800

    init

---
+++ pages/detail1/detail1.vue
@@ -0,0 +1,47 @@
+<template>
+	<view>
+		<swiper indicator-dots circular vertical>
+			<swiper-item>
+				<image src="../../static/1.jpeg" mode="aspectFill"></image>
+			</swiper-item>
+			<swiper-item>
+				<image src="../../static/2.jpeg" mode="aspectFill"></image>
+			</swiper-item>
+			<swiper-item>
+				<image src="../../static/3.jpeg" mode="aspectFill"></image>
+			</swiper-item>
+			<swiper-item>
+				<image src="../../static/4.jpeg" mode="aspectFill"></image>
+			</swiper-item>
+			<swiper-item>
+				<image src="../../static/5.jpeg" mode="aspectFill"></image>
+			</swiper-item>
+		</swiper>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style lang="scss">
+swiper {
+	width: 100vh;
+	height: 200px;
+	border: 1px solid black;
+	view {
+		width: 100%;
+		height: 100%;
+		background: blue;
+	}
+}
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,56 @@
+<template>
+	<view class="container" hover-class="containerHover">
+		<view class="box1" hover-stop-propagation>123</view>
+	</view>
+	<view>---</view>
+	<text selectable>text文字</text>
+	<view>---</view>
+	<scroll-view scroll-x class="scrollView">
+			<view class="box"></view>
+			<view class="box"></view>
+			<view class="box"></view>
+			<view class="box"></view>
+			<view class="box"></view>
+			<view class="box"></view>
+			<view class="box"></view>
+			<view class="box"></view>
+	</scroll-view>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss">
+.container {
+	width: 200px;
+	height: 200px;
+	background-color: blue;
+	.box1 {
+		width: 100px;
+		height: 100px;
+		background: palevioletred;
+	}
+}
+
+.containerHover {
+	width: 300px;
+}
+
+.scrollView {
+	height: 300px;
+	width: 300px;
+	border: 1px solid black;
+	white-space: nowrap;
+		.box {
+			width: 100px;
+			margin: 10px;
+			height: 30px;
+			background-color: rosybrown;
+			display: inline-block;
+		}
+	
+	
+}
+
+</style>
